11ee84d3ae345d46dd529d7d775bfcd268a89bcf
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-09-17 22:27:09 +0200

    added some ts animations

--- TS/script.js
+++ TS/script.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("zobaczmy czy zadziała");
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const overlay = document.querySelector('.overlay');
+const hamMenu = document.querySelector('.ham-menu');
+const hamMenuExit = document.querySelector('.exit');
+hamburgerMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    overlay.classList.toggle('active');
+    hamMenu.animate(animationSpecOpen, animationTiming);
+});
+hamMenuExit.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    overlay.classList.toggle('active');
+});
+const animationSpecOpen = [
+    { width: '0%' },
+    { width: '60%' }
+];
+const animationTiming = {
+    duration: 100,
+    iterations: 1
+};
--- TS/script.ts
+++ TS/script.ts
@@ -1,1 +1,26 @@
-console.log("zobaczmy czy zadziała");
+const hamburgerMenu = document.querySelector('.hamburger-menu') as HTMLElement;
+const overlay = document.querySelector('.overlay') as HTMLElement;
+const hamMenu = document.querySelector('.ham-menu') as HTMLElement;
+const hamMenuExit = document.querySelector('.exit') as HTMLElement;
+
+
+hamburgerMenu.addEventListener('click', () => {
+  hamMenu.classList.toggle('active');
+  overlay.classList.toggle('active');
+  hamMenu.animate(animationSpecOpen, animationTiming);
+});
+
+hamMenuExit.addEventListener('click', () => {
+  hamMenu.classList.toggle('active');
+  overlay.classList.toggle('active');
+});
+
+const animationSpecOpen = [
+  {width: '0%'},
+  {width: '60%'}
+];
+
+const animationTiming = {
+  duration: 100,
+  iterations: 1
+};
