3d401d4d860a0a9c5d5b371da6d46672f090ab02
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-09-17 23:20:01 +0200

    interactivity to sublist in mobile

--- TS/script.js
+++ TS/script.js
@@ -3,6 +3,8 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 const overlay = document.querySelector('.overlay');
 const hamMenu = document.querySelector('.ham-menu');
 const hamMenuExit = document.querySelector('.exit');
+const clickableList = document.querySelectorAll('.clickable-list');
+console.log(clickableList);
 hamburgerMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     overlay.classList.toggle('active');
@@ -11,6 +13,18 @@ hamburgerMenu.addEventListener('click', () => {
 hamMenuExit.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     overlay.classList.toggle('active');
+});
+clickableList.forEach(listItem => {
+    listItem.addEventListener('click', () => {
+        const subList1 = document.querySelector('.sl1');
+        const subList2 = document.querySelector('.sl2');
+        if (listItem.classList.contains('cl1')) {
+            subList1.classList.toggle('active');
+        }
+        else if (listItem.classList.contains('cl2')) {
+            subList2.classList.toggle('active');
+        }
+    });
 });
 const animationSpecOpen = [
     { width: '0%' },
--- TS/script.ts
+++ TS/script.ts
@@ -2,6 +2,9 @@ const hamburgerMenu = document.querySelector('.hamburger-menu') as HTMLElement;
 const overlay = document.querySelector('.overlay') as HTMLElement;
 const hamMenu = document.querySelector('.ham-menu') as HTMLElement;
 const hamMenuExit = document.querySelector('.exit') as HTMLElement;
+const clickableList = document.querySelectorAll('.clickable-list');
+ 
+console.log(clickableList);
 
 
 hamburgerMenu.addEventListener('click', () => {
@@ -14,6 +17,19 @@ hamMenuExit.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
   overlay.classList.toggle('active');
 });
+
+clickableList.forEach(listItem => {
+  listItem.addEventListener('click' , () => {
+    const subList1 = document.querySelector('.sl1') as HTMLElement;
+    const subList2 = document.querySelector('.sl2') as HTMLElement;
+    if(listItem.classList.contains('cl1')) {
+      subList1.classList.toggle('active');
+    } else if(listItem.classList.contains('cl2')) {
+      subList2.classList.toggle('active');
+    }
+  })
+})
+
 
 const animationSpecOpen = [
   {width: '0%'},
